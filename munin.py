55a9bd9d1ac5ced4de020eeefd78fb15b886177d
Maciej Dębski <winemore@staszic.waw.pl>
2013-11-23 17:07:04 +0100

    APC ups plugin.

--- munin.py
+++ munin.py
@@ -17,6 +17,11 @@ class MultiGraphs(object):
             if graph is None or g.name == graph:
                 g.set_value(item, value)
 
+    def update_values(self):
+        """Used if items generate values on their own"""
+        for g in self.graphs:
+            g.update_values()
+
     def str_config(self):
         return '\n'.join(['multigraph %s\n%s' % (g.name, g.str_config())
                           for g in self.graphs])
@@ -37,8 +42,8 @@ class Graph(object):
 
     params = ['title', 'category', 'args', 'vlabel', 'info']
 
-    def __init__(self, name, title=None, category=None, args=None, vlabel='',
-                 info=''):
+    def __init__(self, name, title=None, category=None, args=None, vlabel=None,
+                 info=None):
         self.name = name
         self.items = []
         if title is not None:
@@ -57,6 +62,11 @@ class Graph(object):
         for i in self.items:
             if i.name == item:
                 i.set_value(value)
+
+    def update_values(self):
+        """Used if items generate values on their own"""
+        for i in self.items:
+            i.update_value()
 
     def str_value(self):
         return '\n'.join([i.str_value() for i in self.items])
@@ -85,7 +95,7 @@ def bool2yn(b):
 class Item(object):
     """Single data source"""
 
-    params = ['label', 'type', 'graph', 'cdef', 'min', 'max', 'draw']
+    params = ['label', 'type', 'cdef', 'min', 'max', 'draw']
 
     def __init__(self, name, label='', type=None, graph=True, cdef=None,
                  min=None, max=None, draw=None):
@@ -102,6 +112,10 @@ class Item(object):
 
     def set_value(self, value):
         self.value = value
+
+    def update_value(self):
+        """Override if item shall be responsible for getting value"""
+        raise NotImplementedError()
 
     def str_value(self):
         return '%s.value %s' % (self.name, self.value)
